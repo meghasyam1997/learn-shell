9e35f4729668e3bafb781d6d4c770cdde0e7acd5
meghasyam1997 <meghasyam01@gmail.com>
2023-05-23 12:53:30 +0530

    color

---
+++ echo.sh
@@ -0,0 +1,15 @@
+echo HelloWorld
+
+# Enable Colour
+# echo -e "\e[COLmessage\e[0m"
+# echo -> print given value
+# -e -> enable the \e
+# \e -> enable the COL
+# COL(having six colours in linux) -> Red(31m), Green(32m), yellow(33m), Blue(34m), Magenta(35m) , cyan(36m)
+# message -> what ever we it can be display in that color
+# [0m  -> disable the color
+# All the inputs should be given in Double quotes
+
+echo -e "\e[36m meghasyam\e[0m"
+
+echo hai
